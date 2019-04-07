4cf325afcadee1b2c91258bbf3d251e20b7a2985
zttina <351199191@qq.com>
2019-04-07 21:12:11 +0800

    图片资源添加

--- Example/ZTCameraKit/ZTViewController.m
+++ Example/ZTCameraKit/ZTViewController.m
@@ -2,24 +2,53 @@
 //  ZTViewController.m
 //  ZTCameraKit
 //
-//  Created by hfzdeyx@163.com on 04/05/2019.
-//  Copyright (c) 2019 hfzdeyx@163.com. All rights reserved.
+//  Created by zttina on 04/05/2019.
+//  Copyright (c) 2019 zttina. All rights reserved.
 //
 
 #import "ZTViewController.h"
-
+#import "ZTCameraVC.h"
 @interface ZTViewController ()
+
+@property (nonatomic,strong) UIImageView *imageView;
 
 @end
 
 @implementation ZTViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
+    [self.view addSubview:self.imageView];
+    self.imageView.center = self.view.center;
+    self.imageView.backgroundColor = [UIColor redColor];
+    self.imageView.userInteractionEnabled = YES;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
+    [self.imageView addGestureRecognizer:tap];
 }
+- (void)tap:(UITapGestureRecognizer *)tap {
+    
+    ZTCameraVC *vc = [[ZTCameraVC alloc]init];
+    [self presentViewController:vc animated:YES completion:nil];
+    
+    //
+    @weakify(self);
+    vc.imageBlock = ^(UIImage *image) {
+        @strongify(self);
+        [self dismissViewControllerAnimated:YES completion:nil];
+        self.imageView.image = image;
+    };
+    vc.imageDataBlock = ^(NSData *imageData) {
+        @strongify(self);
+        [self dismissViewControllerAnimated:YES completion:nil];
+        self.imageView.image = [UIImage imageWithData:imageData];
 
+    };
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
